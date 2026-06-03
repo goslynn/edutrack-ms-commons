944be2e63dd7f4baeb657c44ffcdd06e60a11bdf
goslynn <vct.gonzaleza@gmail.com>
2026-06-02 21:53:26 -0400

    elimina clase innecesaria

--- src/main/java/cl/duocuc/edutrack/ms/infrastructure/context/SeededRoleNames.java
+++
@@ -1,7 +0,0 @@
-package cl.duocuc.edutrack.ms.infrastructure.context;
-
-public interface SeededRoleNames {
-    String SUPERUSER = "SUPERUSER";
-    String ADMIN = "ADMIN";
-    String DOCENTE = "DOCENTE";
-}
